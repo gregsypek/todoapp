339cb47c9e54e4492eee4d0abc13efefe2187252
gregsypek <greg.sypek@gmail.com>
2020-04-15 21:30:46 +0200

    add a new feature -sorting task by date

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,6 +4,15 @@ import TaskDone from "./TaskDone";
 
 const TaskList = (props) => {
 	const active = props.tasks.filter((task) => task.active);
+
+	if (active.length >= 2) {
+		active.sort((a, b) => {
+			//zamienia date (string) na liczbe zeby posortowac
+			a = Date.parse(new Date(a.date));
+			b = Date.parse(new Date(b.date));
+			return a - b;
+		});
+	}
 	const activeTasks = active.map((task) => (
 		<Task
 			key={task.id}
@@ -16,6 +25,12 @@ const TaskList = (props) => {
 	// do zaimplementowania na osobnej stronie
 
 	const done = props.tasks.filter((task) => !task.active);
+
+	//sortuje od najmniejszej do najwiekszej liczby
+	if (done.length >= 2) {
+		// console.log(done);
+		done.sort((a, b) => b.finishDate - a.finishDate);
+	}
 	const doneTasks = done.map((task) => (
 		<TaskDone
 			key={task.id}

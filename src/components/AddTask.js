5bf4b2df6bc4e6a210917c8eac9c013de71a0904
gregsypek <greg.sypek@gmail.com>
2020-04-15 17:15:57 +0200

    creating AddTask

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,15 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./AddTask.css";
 
 class AddTask extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div>Dodaj task</div>
-         );
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: "",
+			checked: false,
+			date: "2020-04-20",
+		};
+	}
+	render() {
+		return (
+			<div className="form">
+				<input
+					type="text"
+					placeholder="dodaj zadanie... "
+					value={this.state.text}
+				/>
+				<input type="checkbox" checked={this.state.checked} id="important" />
+				<label htmlFor="important">Priorytet</label>
+				<input type="date" value={this.state.date} />
+				<label htmlFor="date">do kiedy...</label>
+				<button>dodaj</button>
+			</div>
+		);
+	}
 }
- 
+
 export default AddTask;
--- src/components/App.js
+++ src/components/App.js
@@ -68,7 +68,6 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				TODOAPP
 				<AddTask />
 				<TaskList
 					tasks={this.state.tasks}

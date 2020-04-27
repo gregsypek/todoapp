54803efdd8250d65c7a80cb975068394dfcbd8c6
greg <greg@MacBook-Air-greg.local>
2020-04-27 04:02:15 -0700

    Updates

---
+++ precache-manifest.86fb4a0ca1976728fe484bf7097472a7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab1cc3e4ac9d40a7963357881fdae5f",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "b10beb6c561e0fc7c208",
+    "url": "/todoapp/static/css/main.1f816bcd.chunk.css"
+  },
+  {
+    "revision": "aae332b31eeafd95814d",
+    "url": "/todoapp/static/js/2.b7d91aef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoapp/static/js/2.b7d91aef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10beb6c561e0fc7c208",
+    "url": "/todoapp/static/js/main.5a1f30f7.chunk.js"
+  },
+  {
+    "revision": "341bc02fd9708d3fa71b",
+    "url": "/todoapp/static/js/runtime-main.1d40df92.js"
+  },
+  {
+    "revision": "80733e424dbcffcb8d026b7db043730e",
+    "url": "/todoapp/static/media/image2.80733e42.svg"
+  },
+  {
+    "revision": "0ad1ff95c0d197bb56dfe50070b9b993",
+    "url": "/todoapp/static/media/logo.0ad1ff95.svg"
+  },
+  {
+    "revision": "9cd7cd52fe168f1ba53760f0a551ff23",
+    "url": "/todoapp/static/media/ok.9cd7cd52.svg"
+  },
+  {
+    "revision": "d887ba280bb6a17e5d3d36c161da1a5d",
+    "url": "/todoapp/static/media/usun.d887ba28.svg"
+  }
+]);
